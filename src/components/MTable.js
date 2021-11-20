3453c8dda267e3f92f66d848e3e805ea699bdd4b
karananeja <karananeja.r@gmail.com>
2021-11-20 15:39:36 +0530

    data fetching

--- src/components/MTable.js
+++ src/components/MTable.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { data } from '../data';
+import React, { useState, useEffect } from 'react';
+// import { data } from '../data';
 import {
   Table,
   TableBody,
@@ -14,27 +14,43 @@ import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
 import '../css/MTable.css';
 
 const MTable = () => {
-  const [newArray, setNewArray] = useState(data);
+  // const [newArray, setNewArray] = useState(data);
+  const url = 'https://reqres.in/api/users?page=2';
+  const [urlData, setUrlData] = useState([]);
 
-  const handleUpChange = (index) => {
-    let newData = [...newArray];
-    if (index > 0) {
-      let temp = newData[index - 1];
-      newData[index - 1] = newData[index];
-      newData[index] = temp;
-      setNewArray(newData);
-    }
-  };
+  // const handleUpChange = (index) => {
+  //   let newData = [...newArray];
+  //   if (index > 0) {
+  //     let temp = newData[index - 1];
+  //     newData[index - 1] = newData[index];
+  //     newData[index] = temp;
+  //     setNewArray(newData);
+  //   }
+  // };
 
-  const handleDownChange = (index) => {
-    let newData = [...newArray];
-    if (index < newData.length - 1) {
-      let temp = newData[index + 1];
-      newData[index + 1] = newData[index];
-      newData[index] = temp;
-      setNewArray(newData);
-    }
-  };
+  // const handleDownChange = (index) => {
+  //   let newData = [...newArray];
+  //   if (index < newData.length - 1) {
+  //     let temp = newData[index + 1];
+  //     newData[index + 1] = newData[index];
+  //     newData[index] = temp;
+  //     setNewArray(newData);
+  //   }
+  // };
+
+  useEffect(() => {
+    const fetchUrl = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setUrlData(json.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUrl();
+  }, []);
+
   return (
     <div className='table'>
       <TableContainer component={Paper} className='table__container'>
@@ -42,14 +58,13 @@ const MTable = () => {
           <TableHead>
             <TableRow>
               <TableCell className='table__headerCell' />
-              <TableCell className='table__headerCell'>Name</TableCell>
-              <TableCell className='table__headerCell'>Id</TableCell>
-              <TableCell className='table__headerCell'>Email</TableCell>
+              <TableCell className='table__headerCell'>First Name</TableCell>
+              <TableCell className='table__headerCell'>Last Name</TableCell>
               <TableCell className='table__headerCell' />
             </TableRow>
           </TableHead>
           <TableBody>
-            {newArray.map(({ id, name, email }, index) => (
+            {/* {newArray.map(({ id, name, email }, index) => (
               <TableRow key={index}>
                 <TableCell>
                   <ArrowCircleUpIcon onClick={() => handleUpChange(index)} />
@@ -61,6 +76,18 @@ const MTable = () => {
                   <ArrowCircleDownIcon
                     onClick={() => handleDownChange(index)}
                   />
+                </TableCell>
+              </TableRow>
+            ))} */}
+            {urlData.map((data) => (
+              <TableRow key={data.id}>
+                <TableCell>
+                  <ArrowCircleUpIcon />
+                </TableCell>
+                <TableCell>{data['first_name']}</TableCell>
+                <TableCell>{data['last_name']}</TableCell>
+                <TableCell>
+                  <ArrowCircleDownIcon />
                 </TableCell>
               </TableRow>
             ))}
